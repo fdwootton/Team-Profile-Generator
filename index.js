b21f60715b20f54760a22b43e703dc1da0b0c978
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-07 14:19:15 -0500

    added starterHTML

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { COMPLETIONSTATEMENT_TYPES } = require('@babel/types');
+// const { COMPLETIONSTATEMENT_TYPES } = require('@babel/types'); 
 
 const questions = [
     {
@@ -10,10 +10,31 @@ const questions = [
     }
 ];
 
+const starterHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    />
+    <title>My Team</title>
+</head>
+<body>
+    <header class = "jumbotron" style="text-align: center";>
+        <h1>My Team</h1>
+    </header>
+    
+</body>
+</html>`
+
 inquirer
     .prompt(questions)
     .then((response) => {
         console.log(response);
     });
 
-    
+
+
