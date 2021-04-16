a2f43bbb8287f8179d70c5317f4d793a7a08a0ba
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-16 16:32:16 -0500

    Manager.test.js pass

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,9 +1,40 @@
 const Manager = require('../lib/Manager');
  
 describe('Manager', () => {
-  it('should set the name via constructor argument', () => {
-      const testValue = 5;
-      const manager = new Manager ("John", testValue, "John@email.com")
-    expect(manager.id).toBe(testValue);
+
+  describe('Manager', () => {
+    it('should define name via constructor argument and get name via getName() method', () => {
+      const testName = "John";
+      const manager = new Manager ("John")
+      expect(manager.name).toBe(testName);
+      expect(manager.getName()).toBe(testName);
+    });
+  
+    it('should define id via constructor argument and get id via getId() method', () => {
+      const testId = 5;
+      const manager = new Manager ("John", testId);
+      expect(manager.id).toEqual(testId);
+      expect(manager.getId()).toBe(testId);
+    });
+  
+    it('should define email via constructor argument and get email via getEmail() method', () => {
+      const testEmail = "John@test.com";
+      const manager = new Manager ("John", 1 , "John@test.com");
+      expect(manager.email).toBe(testEmail);
+      expect(manager.getEmail()).toBe(testEmail);
+    });
+
+    it('should define office number via constructor argument and get office number via getOfficeNumber() method', () => {
+      const testOfficeNumber = 1234;
+      const manager = new Manager ("John", 1 , "John@test.com", testOfficeNumber);
+      expect(manager.officeNumber).toBe(testOfficeNumber);
+      expect(manager.getOfficeNumber()).toBe(testOfficeNumber);
+    });
+  
+    it('should get role via getRole() method', () => {
+      const testRole = "Manager";
+      const manager = new Manager;
+      expect(manager.getRole()).toBe(testRole);
+    });
   });
 });
