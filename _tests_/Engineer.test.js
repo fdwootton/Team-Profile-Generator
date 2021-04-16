63bbcf446bb40cc4e81c01a05fa92b2c0605429d
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-16 16:01:10 -0500

    Engineer.test.js pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,9 +1,42 @@
 const Engineer = require('../lib/Engineer');
  
 describe('Engineer', () => {
-  it('should set the name via constructor argument', () => {
-      const testValue = 5;
-      const engineer = new Engineer ("John", testValue, "John@email.com")
-    expect(engineer.id).toBe(testValue);
+
+  it('should define name via constructor argument', () => {
+      const testName = "John";
+      const engineer = new Engineer ("John");
+      expect(engineer.name).toBe(testName);
   });
+
+  it('should define id via constructor argument', () => {
+    const testId = 5;
+    const engineer = new Engineer ("John", testId);
+    expect(engineer.id).toEqual(testId);
+  });
+
+  it('should define email via constructor argument', () => {
+    const testEmail = "John@test.com";
+    const engineer = new Engineer ("John", 1 , "John@test.com");
+    expect(engineer.email).toBe(testEmail);
+  });
+
+  it('should define github via constructor argument', () => {
+    const testGithub = "johngithub";
+    const engineer = new Engineer ("John", 1 , "John@test.com", "johngithub");
+    expect(engineer.github).toBe(testGithub);
+  });
+
+  it('should get github username via getGithub() method', () => {
+    const testGithub = "johngithub";
+    const engineer = new Engineer("John", 1 , "John@test.com", "johngithub");
+    expect(engineer.getGithub()).toBe(testGithub);
+  });
+
+  it('should get role via getRole() method', () => {
+    const testRole = "Engineer";
+    const engineer = new Engineer;
+    expect(engineer.getRole()).toBe(testRole);
+  });
+
+
 });
